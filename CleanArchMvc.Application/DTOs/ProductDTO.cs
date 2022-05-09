f70b2fe69f89772fb0614226a02da6fae7355ed3
Pedro Luis <pedrolssilva@hotmail.com>
2022-05-09 18:09:31 -0300

    Created mappings settings profile and DTOs with AutoMapper

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; private set; }
+
+        [Required(ErrorMessage = "The Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString ="{0:C2")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; private set; }
+
+        [Required(ErrorMessage = "The Stock is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; private set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; private set; }
+
+        public Category Category { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+
+    }
+}
