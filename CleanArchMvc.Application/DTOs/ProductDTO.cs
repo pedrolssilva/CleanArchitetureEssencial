5c8433ba1959f659e5aefb46d6b03743c276133b
Pedro Luis <pedrolssilva@hotmail.com>
2022-05-16 19:18:40 -0300

    Created Products controller and view

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -19,23 +19,23 @@ namespace CleanArchMvc.Application.DTOs
         [MinLength(5)]
         [MaxLength(200)]
         [DisplayName("Description")]
-        public string Description { get; private set; }
+        public string Description { get; set; }
 
-        [Required(ErrorMessage = "The Price is required")]
+        [Required(ErrorMessage = "The Price is Required")]
         [Column(TypeName = "decimal(18,2)")]
-        [DisplayFormat(DataFormatString ="{0:C2")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         [DataType(DataType.Currency)]
         [DisplayName("Price")]
-        public decimal Price { get; private set; }
+        public decimal Price { get; set; }
 
         [Required(ErrorMessage = "The Stock is required")]
         [Range(1, 9999)]
         [DisplayName("Stock")]
-        public int Stock { get; private set; }
+        public int Stock { get; set; }
 
         [MaxLength(250)]
         [DisplayName("Product Image")]
-        public string Image { get; private set; }
+        public string Image { get; set; }
 
         public Category Category { get; set; }
 
